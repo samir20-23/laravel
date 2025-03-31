c0e3ae20d078f61cbee1b08435747d5660224999
Samir Aoulad amar <144660881+samir20-23@users.noreply.github.com>
2025-03-31 21:40:51 +0000

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,13 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function index () {
+    return view('home');
+    }
+      public function index2 () {
+    return view('home');
+    }
+      public function index3 () {
+    return view('home');
+    }
 }
